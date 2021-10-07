aa62b0dfcd6738f63fe761c266e48efbb2e80722
brayanbaad <brayan.baa455@gmail.com>
2021-10-06 22:44:17 -0500

    routes editando

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         //$this->call(AreaSeeder::class);
         //$this->call(TeacherSeeder::class);
        // $this->call(QualificationSeeder::class);
-        $this->call(GroupSeeder::class);
+        
 
     }
 }
