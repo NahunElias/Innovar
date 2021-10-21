463cde2083235bfeb17a2315b1c772f8c8a1974f
LuisfernaLara <lfernandolara@unicesar.edu.co>
2021-10-21 14:10:24 -0500

    Preguntas

---
+++ resources/views/Evaluaciones/Preguntas/Respuestas/options.blade.php
@@ -0,0 +1,5 @@
+<td>
+    <button class="btn btn-danger" onclick="Delete_Answer({{ $id }})">
+        <i class="fas fa-eraser"></i>
+    </button>
+</td>
---
+++ resources/views/Evaluaciones/Preguntas/Respuestas/statu.blade.php
@@ -0,0 +1,7 @@
+<td>
+    @if ($estatu)
+        <span class="badge badge-success">Si</span>
+    @else
+        <span class="badge badge-danger">No</span>
+    @endif
+</td>
