3277efb56bb78f21e50e2c3f8b3131c676de918f
gianlucas06 <gianlucasenero6@hotmail.com>
2021-10-26 20:22:17 -0500

    actualizando tarea

--- resources/views/Tarea/create.blade.php
+++ resources/views/Tarea/create.blade.php
@@ -5,10 +5,10 @@
     <div class="card-header border-0">
         <div class="row align-items-center">
             <div class="col">
-                <h3 class="mb-0">Nueva asignatura</h3>
+                <h3 class="mb-0">Nueva tarea</h3>
             </div>
             <div class="col text-right">
-            <a href="{{ url('asignatura') }}" class="btn btn-sm btn-danger">
+            <a href="{{ url('tarea/'.$idclase_a) }}" class="btn btn-sm btn-danger">
                     Cancelar y volver
             </a>
             </div>
@@ -23,41 +23,65 @@
                     @endforeach
                 </div>
         @endif
-        {{-- action="{{ url('asignatura') }}" --}}
-        <form id="form_asignatura" method="post">
+
+        <form id="form_tarea" enctype="multipart/form-data">
             @csrf
+
             <div class="row">
                 <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-md-3"></div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="nom_materia">Nombre de la materia</label>
-                                <input type="text" id="nom_materia" name="nom_materia" class="form-control" value="{{ old('nom_materia') }}" required>
-                            </div>
+                    <div class="form-row">
+                        <input type="hidden" name="idclase" value="{{ $idclase_a }}">
+                        <div class="form-group col-md-6">
+                            <label for="nombre">Titulo</label>
+                            <input type="text" name="nombre" id="nombre" class="form-control" required>
                         </div>
-                        <div class="col-md-3"></div>
-                        <div class="col-md-3"></div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="des_asignatura">Descripción</label>
-                                <textarea name="des_asignatura" id="des_asignatura" cols="30" rows="5" class="form-control">{{ old('des_asignatura') }}</textarea>
-                            </div>
+                        <div class="form-group col-md-6">
+                            <label for="nombre">Archivo</label>
+                            <input type="file" name="archivo" id="archivo" class="form-control">
                         </div>
-                        <div class="col-md-3"></div>
                     </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="edit-descripcion">Descripción</label>
+                            <textarea name="descripcion" id="edit-descripcion" rows="10" cols="100">
+                            </textarea>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="fecha_ini">Fecha inicio</label>
+                            <input type="datetime-local" name="fecha_ini" id="fecha_ini" class="form-control" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="fecha_fin">Fecha final</label>
+                            <input type="datetime-local" name="fecha_fin" id="fecha_fin" class="form-control" required>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
+            <hr>
             <button type="submit" class="btn btn-primary">
                 Guardar
             </button>
 
         </form>
+
     </div>
 </div>
 @endsection
 @section('custom_script')
-<script src="{{ asset('js/asignatura.js') }}"></script>
 
+<script src="{{ asset('js/tarea_option.js') }}"></script>
+<script src="https://cdn.ckeditor.com/ckeditor5/25.0.0/classic/ckeditor.js"></script>
+<script>
+        ClassicEditor
+            .create( document.querySelector( '#edit-descripcion' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    // CKEDITOR.replace('edit-descripcion');
+</script>
 @endsection
